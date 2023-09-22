d3a35c1baf1dc1b96090bfbba4cbc21061797c7d
Nitesh Jain <niteshjainy>
2023-09-22 12:32:26 +0530

    repalced PNG with png

--- src/components/LoginComponent.jsx
+++ src/components/LoginComponent.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Logo from "./../assets/wissenlogo.png";
+import Logo from "./../assets/logo.png";
 const LoginComponent = ({ loginUser }) => {
   return (
     <div className="loginContainer">
